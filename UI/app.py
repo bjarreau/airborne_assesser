0ad1c3a2d299b41d418490ef45472056c90ad1b4
brittney.jarreau <brittney.jarreau@selu.edu>
2021-06-20 19:39:14 -0500

    adding field to supply file path

--- UI/app.py
+++ UI/app.py
@@ -11,6 +11,11 @@ def index():
 def video_feed():
     return Response(stream_client.generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
+@app.route("/handle_source", methods=["POST"])
+def handle_source():
+    video_source = request.form["source_path"]
+    stream_client.set_source(video_source)
+
 if __name__ == "__main__":
     stream_client = VideoStreamer()
     app.run(debug=True, host="0.0.0.0", port=8080)
---
+++ UI/templates/script_src.js
@@ -0,0 +1,7 @@
+function showSource() {
+    if (document.getElementById('cached').checked) {
+        document.getElementById('sourceBox').style.visibility = 'visible';
+    }
+    else document.getElementById('sourceBox').style.visibility = 'hidden';
+
+}
