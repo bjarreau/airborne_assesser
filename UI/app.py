390dd7c6a83e07c8bb0beec170c8235701032775
brittney.jarreau <brittney.jarreau@selu.edu>
2021-06-21 13:47:43 -0500

    video from link now working

--- UI/app.py
+++ UI/app.py
@@ -3,26 +3,21 @@ from streamer import VideoStreamer
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.form.get("source_path") != None:
+        stream_client.set_source(request.form.get("source_path"))
+    elif request.form.get("radius") != None:
+        stream_client.set_radius(request.form.get("radius"))
     return render_template("index.html")
 
 @app.route("/video_feed")
 def video_feed():
     return Response(stream_client.generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-@app.route("/handle_source", methods=["POST"])
-def handle_source():
-    return Response(stream_client.set_source(request.form.get("source_path")), mimetype="multipart/x-mixed-replace; boundary=frame")
-
 @app.route("/get_source", methods=["GET"])
 def get_source():
     return Response(stream_client.get_source(), mimetype="multipart/x-mixed-replace; boundary=frame")
-
-@app.route("/handle_radius", methods=["POST"])
-def handle_radius():
-    stream_client.set_radius(request.form.get("radius"))
-    return Response("ok")
 
 if __name__ == "__main__":
     stream_client = VideoStreamer()
