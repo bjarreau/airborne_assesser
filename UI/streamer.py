3d51dc9e427ea2ee3cbb025b6227f617a32f2442
brittney.jarreau <brittney.jarreau@selu.edu>
2021-06-23 21:34:19 -0500

    start support for webcam

--- UI/streamer.py
+++ UI/streamer.py
@@ -3,6 +3,8 @@ import cv2
 import zmq
 import pafy
 import numpy as np
+import threading
+import time
 from os import getenv
 from dotenv import load_dotenv
 
@@ -11,15 +13,9 @@ load_dotenv()
 class VideoStreamer:
     def __init__(self):
         self.context = zmq.Context()
-        self.footage_socket = self.context.socket(zmq.SUB)
-        self.host = "localhost"
-        self.zmq_port = 5555
-        self.footage_socket.connect('tcp://{}:{}'.format(self.host, self.zmq_port))
-        self.footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
         self.default_radius_size = getenv('DEFAULT_RADIUS')
         self.default_radius_uom = getenv('DEFAULT_RADIUS_UOM')
         self.url = "https://www.youtube.com/watch?v=CmomQkOau7c"
-
         self.active = "Live"
         self.radius_size = getenv('DEFAULT_RADIUS')
         self.radius_uom = getenv('DEFAULT_RADIUS_UOM')
@@ -28,13 +24,25 @@ class VideoStreamer:
         self.message = None
         self.frame_max = 100
         self.RebuildPlayer()
-
-        self.client = "jetson"
-        self.rtsp_port = 8554
         self.width = 1280
         self.height = 720
         self.rtsp_frame = np.zeros((self.height, self.width, 3), np.uint8)
         self.heatmap = np.zeros((self.height, self.width, 3), np.uint8)
+        threading.Thread(target=self.update_live_frame).start()
+
+    def update_live_frame(self):
+        if self.active == "Live":
+            cap = None
+            while True:
+                if not cap:
+                    cap = self.VideoCapture(1, self.width, self.height)
+                success, frame = cap.read()
+                if not success:
+                    cap = None
+                    time.sleep(0.5)
+
+                if frame is not None:
+                    self.rtsp_frame = frame
 
     def getFrame(self):
         self.video.set(cv2.CAP_PROP_POS_FRAMES, self.frame_max)
@@ -44,15 +52,12 @@ class VideoStreamer:
         self.video.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-    def VideoCapture(self, uri, width, height, latency):
+    def VideoCapture(self, dev, width, height):
         self.active = "Live"
-        gst_str = ('rtspsrc location={} latency={} ! '
-                'rtph264depay ! h264parse ! omxh264dec ! '
-                'nvvidconv ! '
+        gst_str = ('v4l2src device=/dev/video{} ! '
                 'video/x-raw, width=(int){}, height=(int){}, '
-                'format=(string)BGRx ! '
-                'videoconvert ! appsink').format(uri, latency, width, height)
-
+                'format=(string)RGB ! '
+                'videoconvert ! appsink').format(dev, width, height)
         return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
 
     def set_source(self, source):
@@ -106,26 +111,13 @@ class VideoStreamer:
 
     def read(self):
         if self.active == "Live":
-           rtsp_frame = self.rtsp_frame.copy()
-           heatmap = self.heatmap.copy()
-
-           if rtsp_frame.shape != (self.height, self.width, 3):
-              print("rtsp shape mismatch", rtsp_frame.shape)
-              return
-
-           if heatmap.shape != (self.height, self.width, 3):
-              print("heatmap shape mismatch", heatmap.shape)
-              return
-
-           frame = cv2.addWeighted(self.rtsp_frame.copy(), 0.7, self.heatmap.copy(), 0.5, 0)
-           return frame
+           return self.rtsp_frame.copy()
         else:
            check, frame = self.video.read()
            if check:
               return frame
 
     def generate(self):
-        encodedImage = None
         while True:
            if not self.paused:
               frame = self.read()
