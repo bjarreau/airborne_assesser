d79b14e24dc63f5f672d3665dfdddcf711a9c8f6
bjarreau <brittney.jarreau@nrlssc.navy.mil>
2021-07-08 20:21:57 -0500

    added initial take on mask detection

--- UI/streamer.py
+++ UI/streamer.py
@@ -12,9 +12,9 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 
 load_dotenv()
-prototxtPath = os.path.sep.join("face_detector", "deploy.prototxt"])
-weightsPath = os.path.sep.join("face_detector", "res10_300x300_ssd_iter_140000.caffemodel"])
-maskNet = load_model("model/mask_detect.model")
+prototxtPath = os.path.sep.join("../model/face_detector", "deploy.prototxt"])
+weightsPath = os.path.sep.join("../model/face_detector", "res10_300x300_ssd_iter_140000.caffemodel"])
+maskNet = load_model("../model/mask_detect.model")
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 class VideoStreamer:
@@ -141,12 +141,37 @@ class VideoStreamer:
             cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
         return frame
 
+    def detect_and_predict_mask(self, frame, faceNet, maskNet):
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
+        faceNet.setInput(blob)
+        detections = faceNet.forward()
+        faces = []
+        locs = []
+        preds = []
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            (startX, startY, endX, endY) = detections[0, 0, i, 3:7] * np.array([w, h, w, h]).astype("int")
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+        if len(faces) > 0:
+            faces = np.array(faces, dtype="float32")
+            preds = maskNet.predict(faces, batch_size=32)
+        return (locs, preds)
+
     def generate(self):
         while True:
            if not self.paused:
               frame = self.read()
               frame = imutils.resize(frame, width=400)
-              (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+              (locs, preds) = self.detect_and_predict_mask(frame, faceNet, maskNet)
               if len(locs) > 0:
                   frame = self.process_faces(locs, preds, frame)
 
