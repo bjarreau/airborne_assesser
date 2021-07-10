aac29b455071ef1733aeefef872abf5a5eae86c9
bjarreau <brittney.jarreau@nrlssc.navy.mil>
2021-07-10 12:10:41 -0500

    Try new camera index

--- UI/VideoStream.py
+++ UI/VideoStream.py
@@ -3,7 +3,7 @@ import cv2
 
 class VideoStream:
     def __init__(self):
-        self.stream = cv2.VideoCapture(0)
+        self.stream = cv2.VideoCapture(-1)
         self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         (self.grabbed, self.frame) = self.stream.read()
