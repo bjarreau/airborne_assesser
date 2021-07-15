3051d8749c2e0264a2dedfc47436cd62925947c6
bjarreau <brittney.jarreau@nrlssc.navy.mil>
2021-07-14 21:17:55 -0500

    change path

--- UI/app.py
+++ UI/app.py
@@ -39,7 +39,7 @@ duration_uom = getenv('DEFAULT_DURATION_UOM')
 #models
 prototxtPath = os.path.sep.join(["./model/face_detector", "deploy.prototxt"])
 weightsPath = os.path.sep.join(["./model/face_detector", "res10_300x300_ssd_iter_140000.caffemodel"])
-maskNet = saved_model("./model/mask_detect.model")
+maskNet = saved_model("./model/mask_detect/")
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 app = Flask(__name__)
