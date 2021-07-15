9e6eb0ab559e614267aa796664436e2610aacdab
brittney.jarreau <brittney.jarreau@selu.edu>
2021-07-14 21:25:48 -0500

    fix import

--- UI/app.py
+++ UI/app.py
@@ -10,9 +10,9 @@ import time
 from os import getenv
 import os
 from dotenv import load_dotenv
+import tensorflow as tf
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import saved_model
 
 load_dotenv()
 outframe = None
@@ -39,7 +39,7 @@ duration_uom = getenv('DEFAULT_DURATION_UOM')
 #models
 prototxtPath = os.path.sep.join(["./model/face_detector", "deploy.prototxt"])
 weightsPath = os.path.sep.join(["./model/face_detector", "res10_300x300_ssd_iter_140000.caffemodel"])
-maskNet = saved_model("./model/mask_detect/")
+maskNet = tf.saved_model.load("./model/mask_detect/")
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 app = Flask(__name__)
