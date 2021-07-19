d2ab7c5532e45921f78ebf714014c6fee8b9f8d6
brittney.jarreau <brittney.jarreau@selu.edu>
2021-07-19 13:59:27 -0500

    better

--- UI/app.py
+++ UI/app.py
@@ -9,19 +9,7 @@ import time
 from os import getenv
 import os
 from dotenv import load_dotenv
-#import tensorflow as tf
-#from tensorflow.python.saved_model import tag_constants
-#from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
-#from tensorflow.keras.preprocessing.image import img_to_array
-#from tensorflow.keras.models import load_model
 
-#physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#if len(physical_devices) > 0:
-#  for device in physical_devices:
-#      tf.config.experimental.set_memory_growth(device, True)
-#      tf.config.experimental.set_per_process_gpu_memory_fraction = 0.4
-
-#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 load_dotenv()
 outframe = None
 url = "https://youtu.be/7PYzSXHd6U4"
@@ -42,16 +30,9 @@ radius_uom = getenv('DEFAULT_RADIUS_UOM')
 duration = getenv('DEFAULT_DURATION')
 duration_uom = getenv('DEFAULT_DURATION_UOM')
 
-#models
-#maskNet = load_model("./model/mask_detect")
-#maskNet = tf.saved_model.load("./model/mask_detect/TRT", tags=[tag_constants.SERVING])
-#maskNet_funcs = maskNet.signatures["serving_default"]
-#frozen_func = convert_to_constants.convert_variables_to_constants_v2(maskNet_funcs)
-#face_cascade = cv2.CascadeClassifier("./model/haarcascade_frontalface_alt2.xml")
-#profile_cascade = cv2.CascadeClassifier("./model/haarcascade_profileface.xml")
+
 weightsPath = "./model/mask_detect/yolov4-tiny-mask.weights"
 configPath = "./model/mask_detect/yolov4-tiny-mask.cfg"
-
 maskNetCv2 = cv2.dnn_DetectionModel(configPath, weightsPath)
 maskNetCv2.setInputSize(416, 416)
 maskNetCv2.setInputScale(1.0 / 255)
@@ -60,7 +41,6 @@ maskNetCv2.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 maskNetCv2.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -116,16 +96,13 @@ def get_duration():
     return "{} {}".format(duration, duration_uom)
 
 def find_masks(frame):
-    (h, w) = frame.shape[:2]
-    scale = 300/float(w)
-    frame = cv2.resize(frame, (300, int(h*scale)), interpolation=cv2.INTER_AREA)
     classes, confidences, boxes = maskNetCv2.detect(frame, 0.5, 0.5)
     for cl, score, (left, top, width, height) in zip(classes, confidences, boxes):
         if score[0] > 0.5:
             start_point = (int(left), int(top))
             end_point = (int(left + width), int(top + height))
             color = (0, 0, 255) if cl else (0, 255, 0)
-            label = "MASK" if cl else "NO MASK"
+            label = "No MASK" if cl else "MASK"
             img = cv2.rectangle(frame, start_point, end_point, color, 2)  # draw class box
             text = "{}:{:.2f}".format(label, score[0])
             cv2.putText(frame, text, start_point, cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)  # print class type with score
@@ -134,12 +111,18 @@ def find_masks(frame):
 
 def generate():
     encoded = None
+    c=0
     while True:
         (conf, frame) = livestream.read() if active == "Live" else linkedstream.read()
+        c+=1
         if frame is not None:
-            frame = find_masks(frame)
-            (f, encoded) = cv2.imencode(".jpg", frame)
-            yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encoded) + b'\r\n')
+            (h, w) = frame.shape[:2]
+            scale = 300/float(w)
+            frame = cv2.resize(frame, (300, int(h*scale)), interpolation=cv2.INTER_AREA)
+            if c%5 == 0:
+                frame = find_masks(frame)
+                (f, encoded) = cv2.imencode(".jpg", frame)
+                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encoded) + b'\r\n')
 
 @app.route("/video_feed")
 def video_feed():
