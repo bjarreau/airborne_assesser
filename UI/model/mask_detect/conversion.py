d63874aa43885f66cf9c2d7aabe94947f3b2a803
bjarreau <brittney.jarreau@nrlssc.navy.mil>
2021-07-19 07:27:28 -0500

    trying to make TF-TRT model

--- UI/model/mask_detect/conversion.py
+++ UI/model/mask_detect/conversion.py
@@ -1,4 +1,7 @@
-from tensorflow.contrib.tensorrt import trt_convert as trt
-converter = trt.TrtGraphConverterV2(input_saved_model_dir=".")
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+
+convert_params = trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(precision_mode=trt.TrtPrecisionMode.FP32, max_workspace_size_byte=8000000000)
+converter = trt.TrtGraphConverterV2(input_saved_model_dir='.', conversion_params=convert_params)
 converter.convert()
-converter.save(".")
+converter.save("./TRT")
+print('Done Converting to TF-TRT FP32')
