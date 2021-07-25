881c1676334e4adeeddbaafb52d5a297a80ec69c
bjarreau <brittney.jarreau@nrlssc.navy.mil>
2021-07-25 16:49:58 -0500

    try a heatmap render

---
+++ UI/heatmap_generator.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import time
+
+class HMap:
+    def __init__(self, width, height, fade_interval=2, radius=20):
+        self.width = width
+        self.height = height
+        self.accum_image = np.zeros((self.height, self.width), np.uint8)
+        self.heatmap = np.zeros((self.height, self.width), np.uint8)
+        self.radius = radius
+        
+        self.fade_interval = fade_interval
+        self.st = time.time()
+
+    def apply_color_map(self, image, x1, y1):
+        if time.time() - self.st > self.fade_interval:
+            self.st = time.time()
+            self.accum_image = cv2.addWeighted(self.accum_image, 1, self.accum_image, 0, float(-2))
+            self.accum_image = cv2.blur(self.accum_image, (55,55))
+
+        # create a mask from image and add it to accum_image
+        mask = np.zeros((self.height, self.width), np.uint8)
+        mask = cv2.circle(mask, (x1+20,y1+20), self.radius, (75,75,75), -1)
+        mask = cv2.blur(mask, (105,105), cv2.BORDER_DEFAULT)
+
+        self.accum_image = cv2.add(self.accum_image, mask)
+        self.heatmap = cv2.applyColorMap(self.accum_image, cv2.COLORMAP_JET)
+        return self.heatmap
