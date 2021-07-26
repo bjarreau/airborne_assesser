2ad35b7746301a2d5a3f970b2e5b4769ef9828d1
bjarreau <brittney.jarreau@selu.edu>
2021-07-26 02:18:22 +0000

    update function

--- UI/my_heatmap_generator.py
+++ UI/my_heatmap_generator.py
@@ -3,24 +3,21 @@ import numpy as np
 import time
 
 class HMap:
-    def __init__(self, width, height, fade_interval=2, radius=20):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
         self.accum_image = np.zeros((self.height, self.width), np.uint8)
         self.heatmap = np.zeros((self.height, self.width), np.uint8)
-        self.radius = radius
-        
-        self.fade_interval = fade_interval
         self.st = time.time()
 
-    def apply_color_map(self, image, x1, y1):
-        if time.time() - self.st > self.fade_interval:
+    def apply_color_map(self, image, x1, y1, radius, duration):
+        if time.time() - self.st > duration:
             self.accum_image = cv2.addWeighted(self.accum_image, 1, self.accum_image, 0, float(-2))
             self.accum_image = cv2.blur(self.accum_image, (55,55))
 
         # create a mask from image and add it to accum_image
         mask = np.zeros((self.height, self.width), np.uint8)
-        mask = cv2.circle(mask, (x1+20, y1+20), self.radius, (75,75,75), -1)
+        mask = cv2.circle(mask, (x1+20, y1+20), radius, (75,75,75), -1)
         mask = cv2.blur(mask, (105,105), cv2.BORDER_DEFAULT)
 
         self.accum_image = cv2.add(self.accum_image, mask)
