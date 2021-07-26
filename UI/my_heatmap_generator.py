0ecfdc66a64ae86b19f8a60ef2aec940a17a0890
bjarreau <brittney.jarreau@selu.edu>
2021-07-26 13:26:20 +0000

    save heatmap progress

--- UI/my_heatmap_generator.py
+++ UI/my_heatmap_generator.py
@@ -6,8 +6,8 @@ class HMap:
     def __init__(self, width, height, x1, y1, classification):
         self.width = width
         self.height = height
-        self.accum_image = np.zeros((self.height, self.width), np.uint8)
-        self.heatmap = np.zeros((self.height, self.width), np.uint8)
+        #
+        #self.heatmap = np.zeros((self.height, self.width), np.uint8)
         self.cx = x1
         self.cy = y1
         self.label = classification
@@ -15,7 +15,7 @@ class HMap:
         self.intensity = 0.2
 
     def apply_color_map(self, image, radius, duration):
-        if (time.time() - self.st) > duration:
+        if (time.time() - self.st) < duration:
             print(time.time() - self.st)
             #self.accum_image = cv2.addWeighted(self.accum_image, 1, self.accum_image, 0, float(-2))
             #self.accum_image = cv2.blur(self.accum_image, (55,55))
@@ -24,9 +24,10 @@ class HMap:
             mask = np.zeros((self.height, self.width), np.uint8)
             mask = cv2.circle(mask, (self.cx+20, self.cy+20), radius, (75,75,75), -1)
             mask = cv2.blur(mask, (105,105), cv2.BORDER_DEFAULT)
-
-            self.accum_image = cv2.add(self.accum_image, mask)
-            self.heatmap = cv2.applyColorMap(self.accum_image, cv2.COLORMAP_JET)
+            
+            #self.accum_image = np.zeros((self.height, self.width), np.uint8)
+            #self.accum_image = cv2.add(self.accum_image, mask)
+            self.heatmap = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
             image = cv2.addWeighted(np.array(image), 0.9, self.heatmap, self.intensity, 0)
             self.intensity = self.intensity - (self.intensity/duration)
         return image
