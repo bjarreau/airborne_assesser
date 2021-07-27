284a22040d2704b0052b0682906df9a80362c8de
bjarreau <brittney.jarreau@selu.edu>
2021-07-27 20:07:23 +0000

    update scaling to help mask detect

--- UI/my_heatmap_generator.py
+++ UI/my_heatmap_generator.py
@@ -9,18 +9,15 @@ class HMap:
         self.box = box
         self.st = time.time()
         self.label = label
-        if label == 0:
-            self.opacity = 80
-        else:
-            self.opacity = 60
+        self.opacity = 50
 
     def draw_map(self, frame, radius, duration):
         delta = time.time() - self.st
         radius = radius
         if delta < duration:
             self.opacity = int(self.opacity*((duration-delta)/duration))
-            hue = "red" if (self.label == 0) else "green"
-            if self.label == 1:
+            hue = "red" if (self.label == 1) else "green"
+            if self.label == 0:
                 radius = radius/2
             overlay_image = PIL.Image.new("RGB", frame.size, color=hue)
             img = PIL.Image.new("L", frame.size, color=0) 
@@ -28,7 +25,7 @@ class HMap:
             x = int((self.box[0] + self.box[0] + self.box[2])/2)
             y = int((self.box[1] + self.box[1] + self.box[3])/2)
             draw.ellipse([self.box[0]-radius, 
-                          self.box[1]-radius, 
+                          self.box[1]-(radius/2), 
                           self.box[2]+radius, 
                           self.box[3]+radius], fill=self.opacity, outline=None)
             mask_image = img.filter(ImageFilter.GaussianBlur(radius=10))
