6b596c765422137ddc8416165de8ad1d03a9c72b
bjarreau <brittney.jarreau@selu.edu>
2021-07-27 17:45:18 +0000

    getting better

--- UI/my_heatmap_generator.py
+++ UI/my_heatmap_generator.py
@@ -9,25 +9,28 @@ class HMap:
         self.box = box
         self.st = time.time()
         self.label = label
-        if label == 0:
+        if label == 1:
             self.opacity = 128
         else:
             self.opacity = 100
 
     def draw_map(self, frame, radius, duration):
         delta = time.time() - self.st
+        radius = radius * 5
         if delta < duration:
             self.opacity = int(self.opacity*((duration-delta)/duration))
-            hue = "red" if (self.label == 0) else "green"
-            if self.label == 1:
+            hue = "red" if (self.label == 1) else "green"
+            if self.label == 0:
                 radius = radius/2
             overlay_image = PIL.Image.new("RGB", frame.size, color=hue)
             img = PIL.Image.new("L", frame.size, color=0) 
             draw = ImageDraw.Draw(img)
-            draw.ellipse([self.box.left()-radius, 
-                          self.box.bottom()-radius, 
-                          self.box.right()+radius, 
-                          self.box.top()+radius], fill=self.opacity, outline=None)
+            x = int((self.box[0] + self.box[0] + self.box[2])/2)
+            y = int((self.box[1] + self.box[1] + self.box[3])/2)
+            draw.ellipse([x-radius, 
+                          y-radius, 
+                          x+radius, 
+                          y+radius], fill=self.opacity, outline=None)
             mask_image = img.filter(ImageFilter.GaussianBlur(radius=10))
             frame = PIL.Image.composite(overlay_image, frame, mask_image)
         return frame
